fe870a389f9c91a824f55237ccd8f1430eb96c8b
Alice P. <alicepav231@gmail.com>
2026-03-27 20:47:01 +0100

    Update pir_alarm.ino

--- pir_alarm.ino
+++ pir_alarm.ino
@@ -1,1 +1,29 @@
+const int pirPin = 2;
+const int buzzer = 9;
+const int led = 13;
+int pirState = LOW;
 
+void setup() 
+{
+  pinMode(pirPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  pirState = digitalRead(pirPin);
+  if (pirState == HIGH) 
+  {
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(led, HIGH);
+    Serial.println("Movement detected!");
+  } 
+  else 
+  {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, LOW);
+  }
+  delay(200);
+}
