1d0fb3b248eda8a6a1e29162354e62c65aa94faf
Alice P. <alicepav231@gmail.com>
2026-03-27 20:48:10 +0100

    Update mini_robot.ino

--- mini_robot.ino
+++ mini_robot.ino
@@ -1,1 +1,25 @@
+#include <Servo.h>
 
+Servo leftServo;
+Servo rightServo;
+
+void setup() 
+{
+  leftServo.attach(9);
+  rightServo.attach(10);
+}
+
+void loop() 
+{
+  leftServo.write(180);
+  rightServo.write(0);
+  delay(2000);
+
+  leftServo.write(0);
+  rightServo.write(180);
+  delay(2000);
+
+  leftServo.write(90);
+  rightServo.write(90);
+  delay(1000);
+}
