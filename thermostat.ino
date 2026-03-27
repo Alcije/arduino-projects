316b9a5d599bbf9091e6ab9302cb6220cad77305
Alice P. <alicepav231@gmail.com>
2026-03-27 21:25:34 +0100

    Update thermostat.ino

--- thermostat.ino
+++ thermostat.ino
@@ -1,1 +1,19 @@
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+const int relay = 3;
 
+void setup()
+{
+  dht.begin();
+  pinMode(relay, OUTPUT);
+}
+
+void loop()
+{
+  float temp = dht.readTemperature();
+  if(temp>25) digitalWrite(relay,HIGH); // ventiler
+  else digitalWrite(relay,LOW);
+  delay(1000);
+}
