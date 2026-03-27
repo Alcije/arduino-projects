71e0323185b0602600de8f842d63ca0b658bb927
Alice P. <alicepav231@gmail.com>
2026-03-27 20:45:51 +0100

    Update temperature_sensor.ino

--- temperature_sensor.ino
+++ temperature_sensor.ino
@@ -1,1 +1,17 @@
+const int tempPin = A0;
 
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int val = analogRead(tempPin);
+  float voltage = val * 5.0 / 1023.0;
+  float temperatureC = voltage * 100; // LM35
+  Serial.print("Temperature: ");
+  Serial.print(temperatureC);
+  Serial.println(" C");
+  delay(1000);
+}
