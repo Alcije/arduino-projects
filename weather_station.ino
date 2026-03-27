aebe36d9d01cc5ee209af43078c983528b9a1518
Alice P. <alicepav231@gmail.com>
2026-03-27 21:23:55 +0100

    Update weather_station.ino

--- weather_station.ino
+++ weather_station.ino
@@ -1,1 +1,30 @@
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+const int ledGreen = 3;
+const int ledRed = 4;
 
+void setup() 
+{
+  dht.begin();
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  float t = dht.readTemperature();
+  if(t>28 || t<20)
+  {
+    digitalWrite(ledRed,HIGH);
+    digitalWrite(ledGreen,LOW);
+  }
+  else
+  {
+    digitalWrite(ledRed,LOW);
+    digitalWrite(ledGreen,HIGH);
+  }
+  delay(1000);
+}
